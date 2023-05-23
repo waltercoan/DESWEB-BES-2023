04ef6b2a8672276e99270dc9aeba82148cf955c6
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-05-23 19:46:50 -0300

    intro js

---
+++ introjs/introjs.js
@@ -0,0 +1,12 @@
+//alert("Eu não acredito...")
+
+/*
+var resposta = confirm("Voce esta certo disso?")
+alert("Sua resposta foi: " + resposta)
+console.log(resposta)
+console.debug(resposta)*/
+/*
+var nome = prompt("Digite o seu nome")
+//window.document.writeln("Voce digitou: " + nome)
+document.writeln("Voce digitou: " + nome)
+*/
