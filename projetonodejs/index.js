8bc10ecfaa3db2434f424f89765fe152d3aadbfc
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-06-20 19:46:56 -0300

    aplicando bootstrap

--- projetonodejs/index.js
+++ projetonodejs/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const { engine } = require('express-handlebars')
+const path = require('path')
 
 app.set('view engine', 'handlebars')
 app.engine('handlebars', engine(''))
+
+app.use('/css', express.static(path.join(__dirname,'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname,'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname,'node_modules/jquery/dist')))
+app.use('/public', express.static(path.join(__dirname,'public')))
 
 app.get('/', function(req,res){
     //res.send('Eu não acredito')
